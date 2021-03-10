f2ce748591d5d21f17b39d51affeec92d927cf54
Shreenivas-R <49265868+Shreenivas-R@users.noreply.github.com>
2021-03-10 16:21:28 +0530

    Update countries-ser.service.ts

--- angular-8/src/app/service/countries-ser.service.ts
+++ angular-8/src/app/service/countries-ser.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesSerService {
 
-   baseUrl = 'http://localhost:3000/api/v1/corona?countries=India, Nepal, Bangladesh, Pakistan, Bhutan, Sri Lanka, Maldives';
+   baseUrl = environment.baseUrl;;
   constructor(private https: HttpClient) { }
-  
   
   public getMultipleCountries(): Observable<any>{
   return this.https.get<any>(this.baseUrl).pipe(
